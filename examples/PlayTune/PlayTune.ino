b4707dc5460071698a0ff596e25c7e0d7abcc2c3
Scott Allen <saydisp-git@yahoo.ca>
2017-02-07 09:57:32 -0500

    Fix comment in PlayTune sketch: Devkit -> DevKit

--- examples/PlayTune/PlayTune.ino
+++ examples/PlayTune/PlayTune.ino
@@ -142,7 +142,7 @@ void setup()
   // if not a DevKit
   tunes.initChannel(PIN_SPEAKER_2);
 #else
-  // if it's a Devkit
+  // if it's a DevKit
   tunes.initChannel(PIN_SPEAKER_1); // use the same pin for both channels
   tunes.toneMutesScore(true);       // mute the score when a tone is sounding
 #endif
