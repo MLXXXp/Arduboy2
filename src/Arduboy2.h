4ff6973a4b8e17422210f7c00c0b49507fe63244
Scott Allen <saydisp-git@yahoo.ca>
2017-03-22 18:16:10 -0400

    Change version to 3.1.1

--- src/Arduboy2.h
+++ src/Arduboy2.h
@@ -32,7 +32,7 @@
  * #endif
  * \endcode
  */
-#define ARDUBOY_LIB_VER 30100
+#define ARDUBOY_LIB_VER 30101
 
 // EEPROM settings
 #define ARDUBOY_UNIT_NAME_LEN 6 /**< The maximum length of the unit name string. */
