58ae2564123e5af6ef0b1a9b48338d7f744b550c
Scott Allen <saydisp-git@yahoo.ca>
2018-04-06 15:16:26 -0400

    Change version to 5.1.0

--- src/Arduboy2.h
+++ src/Arduboy2.h
@@ -34,7 +34,7 @@
  * #endif
  * \endcode
  */
-#define ARDUBOY_LIB_VER 50000
+#define ARDUBOY_LIB_VER 50100
 
 // EEPROM settings
 #define ARDUBOY_UNIT_NAME_LEN 6 /**< The maximum length of the unit name string. */
